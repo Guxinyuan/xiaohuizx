6ca1c249d2f1e067b9776100648105ea66214140
顾新媛 <guxinyuan@guxinyuandeMacBook-Pro.local>
2018-09-27 01:05:48 +0800

    fifth

--- form.js
+++ form.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 			$('#zhiyuanyi2').append("<option value='办公室'>办公室</option>");
 			$('#zhiyuanyi2').append("<option value='实践部'>实践部</option>");
 			$('#zhiyuanyi2').append("<option value='文化事业部'>文化事业部</option>");
-			$('#zhiyuanyi2').append("<option value='网络与新媒体部'>网络与新媒体部</option>");
+			$('#zhiyuanyi2').append("<option value='网络与新媒体运营部'>网络与新媒体运营部</option>");
 		}
 		else if(v1 == '校学生会'){
 			$("#zhiyuanyi2").empty();
@@ -60,9 +60,9 @@ $(document).ready(function(){
 		else if (v1 == '校研究生会') {
 			$("#zhiyuanyi2").empty();
 			$('#zhiyuanyi2').append("<option value='办公室'>办公室</option>");
-			$('#zhiyuanyi2').append("<option value='学术实践部'>学术实践部</option>");
+			$('#zhiyuanyi2').append("<option value='学实部'>学实部</option>");
 			$('#zhiyuanyi2').append("<option value='外联部'>外联部</option>");
-			$('#zhiyuanyi2').append("<option value='期刊编辑部'>期刊编辑部</option>");
+			$('#zhiyuanyi2').append("<option value='期刊部'>期刊部</option>");
 			$('#zhiyuanyi2').append("<option value='宣传部'>宣传部</option>");
 			$('#zhiyuanyi2').append("<option value='文体部'>文体部</option>");
 		}
@@ -81,20 +81,20 @@ $(document).ready(function(){
 			$('#zhiyuanyi2').append("<option value='有事大家谈'>有事大家谈节目组</option>");
 			$('#zhiyuanyi2').append("<option value='魅力第六天'>魅力第六天节目组</option>");
 			$('#zhiyuanyi2').append("<option value='ICQ Station'>ICQ Station节目组</option>");
-			$('#zhiyuanyi2').append("<option value='生活MIX'>生活MIX节目组</option>");
+			$('#zhiyuanyi2').append("<option value='生活Mix'>生活Mix节目组</option>");
 			$('#zhiyuanyi2').append("<option value='专题组'>专题组</option>");
 			$('#zhiyuanyi2').append("<option value='非常点播'>非常点播节目组</option>");
 			$('#zhiyuanyi2').append("<option value='行政部'>行政部</option>");
 			$('#zhiyuanyi2').append("<option value='外联部'>外联部</option>");
 			$('#zhiyuanyi2').append("<option value='活动部'>活动部</option>");
 			$('#zhiyuanyi2').append("<option value='新媒体部'>新媒体部</option>");
-			$('#zhiyuanyi2').append("<option value='台宣部（图文、视频）'>台宣部（图文、视频）</option>");
+			$('#zhiyuanyi2').append("<option value='台宣部'>台宣部</option>");
 			
 			
 		}
-		else if (v1 == '校礼仪协会') {
+		else if (v1 == '礼仪协会') {
 			$("#zhiyuanyi2").empty();
-			$('#zhiyuanyi2').append("<option value='礼仪部'>礼仪部</option>");
+			$('#zhiyuanyi2').append("<option value='培训部'>培训部</option>");
 			$('#zhiyuanyi2').append("<option value='宣传部'>宣传部</option>");
 			$('#zhiyuanyi2').append("<option value='公关部'>公关部</option>");
 			$('#zhiyuanyi2').append("<option value='行政部'>行政部</option>");
@@ -112,7 +112,7 @@ $('#zhiyuaner').change(function(){
 			$('#zhiyuaner2').append("<option value='办公室'>办公室</option>");
 			$('#zhiyuaner2').append("<option value='实践部'>实践部</option>");
 			$('#zhiyuaner2').append("<option value='文化事业部'>文化事业部</option>");
-			$('#zhiyuaner2').append("<option value='网络与新媒体部'>网络与新媒体部</option>");
+			$('#zhiyuaner2').append("<option value='网络与新媒体运营部'>网络与新媒体运营部</option>");
 		}
 		else if(v2 == '校学生会'){
 			$("#zhiyuaner2").empty();
@@ -160,9 +160,9 @@ $('#zhiyuaner').change(function(){
 		else if (v2 == '校研究生会') {
 			$("#zhiyuaner2").empty();
 			$('#zhiyuaner2').append("<option value='办公室'>办公室</option>");
-			$('#zhiyuaner2').append("<option value='学术实践部'>学术实践部</option>");
+			$('#zhiyuaner2').append("<option value='学实部'>学实部</option>");
 			$('#zhiyuaner2').append("<option value='外联部'>外联部</option>");
-			$('#zhiyuaner2').append("<option value='期刊编辑部'>期刊编辑部</option>");
+			$('#zhiyuaner2').append("<option value='期刊部'>期刊部</option>");
 			$('#zhiyuaner2').append("<option value='宣传部'>宣传部</option>");
 			$('#zhiyuaner2').append("<option value='文体部'>文体部</option>");
 		}
@@ -181,20 +181,20 @@ $('#zhiyuaner').change(function(){
 			$('#zhiyuaner2').append("<option value='有事大家谈'>有事大家谈节目组</option>");
 			$('#zhiyuaner2').append("<option value='魅力第六天'>魅力第六天节目组</option>");
 			$('#zhiyuaner2').append("<option value='ICQ Station'>ICQ Station节目组</option>");
-			$('#zhiyuaner2').append("<option value='生活MIX'>生活MIX节目组</option>");
+			$('#zhiyuaner2').append("<option value='生活Mix'>生活Mix节目组</option>");
 			$('#zhiyuaner2').append("<option value='专题组'>专题组</option>");
 			$('#zhiyuaner2').append("<option value='非常点播'>非常点播节目组</option>");
 			$('#zhiyuaner2').append("<option value='行政部'>行政部</option>");
 			$('#zhiyuaner2').append("<option value='外联部'>外联部</option>");
 			$('#zhiyuaner2').append("<option value='活动部'>活动部</option>");
 			$('#zhiyuaner2').append("<option value='新媒体部'>新媒体部</option>");
-			$('#zhiyuaner2').append("<option value='台宣部（图文、视频节目组）'>台宣部（图文、视频）</option>");
+			$('#zhiyuaner2').append("<option value='台宣部'>台宣部</option>");
 			
 			
 		}
-		else if (v2 == '校礼仪协会') {
+		else if (v2 == '礼仪协会') {
 			$("#zhiyuaner2").empty();
-			$('#zhiyuaner2').append("<option value='礼仪部'>礼仪部</option>");
+			$('#zhiyuaner2').append("<option value='培训部'>培训部</option>");
 			$('#zhiyuaner2').append("<option value='宣传部'>宣传部</option>");
 			$('#zhiyuaner2').append("<option value='公关部'>公关部</option>");
 			$('#zhiyuaner2').append("<option value='行政部'>行政部</option>");
@@ -211,7 +211,7 @@ $('#zhiyuansan').change(function(){
 			$('#zhiyuansan2').append("<option value='办公室'>办公室</option>");
 			$('#zhiyuansan2').append("<option value='实践部'>实践部</option>");
 			$('#zhiyuansan2').append("<option value='文化事业部'>文化事业部</option>");
-			$('#zhiyuansan2').append("<option value='网络与新媒体部'>网络与新媒体部</option>");
+			$('#zhiyuansan2').append("<option value='网络与新媒体运营部'>网络与新媒体运营部</option>");
 		}
 		else if(v3 == '校学生会'){
 			$("#zhiyuansan2").empty();
@@ -251,7 +251,7 @@ $('#zhiyuansan').change(function(){
 			$("#zhiyuansan2").empty();
 			$('#zhiyuansan2').append("<option value='爱心部'>爱心部</option>");
 			$('#zhiyuansan2').append("<option value='新媒体部'>新媒体部</option>");
-			$('#zhiyuansan2').append("<option value='同伴教育部'>同伴部</option>");
+			$('#zhiyuansan2').append("<option value='同伴教育部'>同伴教育部</option>");
 			$('#zhiyuansan2').append("<option value='外联部'>外联部</option>");
 			$('#zhiyuansan2').append("<option value='宣传部'>宣传部</option>");
 			$('#zhiyuansan2').append("<option value='应急救援部'>应急救援部</option>");
@@ -259,9 +259,9 @@ $('#zhiyuansan').change(function(){
 		else if (v3 == '校研究生会') {
 			$("#zhiyuansan2").empty();
 			$('#zhiyuansan2').append("<option value='办公室'>办公室</option>");
-			$('#zhiyuansan2').append("<option value='学术实践部'>学术实践部</option>");
+			$('#zhiyuansan2').append("<option value='学实部'>学实部</option>");
 			$('#zhiyuansan2').append("<option value='外联部'>外联部</option>");
-			$('#zhiyuansan2').append("<option value='期刊编辑部'>期刊编辑部</option>");
+			$('#zhiyuansan2').append("<option value='期刊部'>期刊部</option>");
 			$('#zhiyuansan2').append("<option value='宣传推广部'>宣传推广部</option>");
 			$('#zhiyuansan2').append("<option value='文体部'>文体部</option>");
 		}
@@ -280,20 +280,20 @@ $('#zhiyuansan').change(function(){
 			$('#zhiyuansan2').append("<option value='有事大家谈'>有事大家谈节目组</option>");
 			$('#zhiyuansan2').append("<option value='魅力第六天'>魅力第六天节目组</option>");
 			$('#zhiyuansan2').append("<option value='ICQ Station'>ICQ Station节目组</option>");
-			$('#zhiyuansan2').append("<option value='生活MIX节目组'>生活MIX节目组</option>");
+			$('#zhiyuansan2').append("<option value='生活Mix节目组'>生活Mix节目组</option>");
 			$('#zhiyuansan2').append("<option value='专题组'>专题组</option>");
 			$('#zhiyuansan2').append("<option value='非常点播'>非常点播节目组</option>");
 			$('#zhiyuansan2').append("<option value='行政部'>行政部</option>");
 			$('#zhiyuansan2').append("<option value='外联部'>外联部</option>");
 			$('#zhiyuansan2').append("<option value='活动部'>活动部</option>");
 			$('#zhiyuansan2').append("<option value='新媒体部'>新媒体部</option>");
-			$('#zhiyuansan2').append("<option value='台宣部（图文、视频）'>台宣部（图文、视频）</option>");
+			$('#zhiyuansan2').append("<option value='台宣部'>台宣部</option>");
 			
 			
 		}
-		else if (v3 == '校礼仪协会') {
+		else if (v3 == '礼仪协会') {
 			$("#zhiyuansan2").empty();
-			$('#zhiyuansan2').append("<option value='礼仪部'>礼仪部</option>");
+			$('#zhiyuansan2').append("<option value='培训部'>培训部</option>");
 			$('#zhiyuansan2').append("<option value='宣传部'>宣传部</option>");
 			$('#zhiyuansan2').append("<option value='公关部'>公关部</option>");
 			$('#zhiyuansan2').append("<option value='行政部'>行政部</option>");
